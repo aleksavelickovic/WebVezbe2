11855d86b064732aac3c4d865d764fd4b7dc9b2a
aleksavelickovic <velickovic.sr44.2023@uns.ac.rs>
2024-10-21 21:39:36 +0200

    WIP izmena osobe

--- Vezbe2/src/main/java/servlets/IzmenaOsobe.java
+++ Vezbe2/src/main/java/servlets/IzmenaOsobe.java
@@ -1,11 +1,16 @@
 package servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Osoba;
 
 /**
  * Servlet implementation class IzmenaOsobe
@@ -26,8 +31,28 @@ public class IzmenaOsobe extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		ServletContext servletContext = getServletContext();
+		ArrayList<Osoba> osobe = (ArrayList<Osoba>) servletContext.getAttribute("osobe");
+		
+		response.getWriter().append("<!DOCTYPE html>\r\n"
+				+ "<html>\r\n"
+				+ "<head>\r\n"
+				+ "<meta charset=\"UTF-8\">\r\n"
+				+ "<title>Izmena osobe</title>\r\n"
+				+ "</head>\r\n"
+				+ "<body>\r\n"
+				+ "<h1>izmeni ovu osobu: </h1>\r\n"
+				+ "\r\n"
+				+ "<form action=\"IzmenaOsobe\" method=\"POST\">\r\n"
+				+ "	<label>Ime:</label>\r\n"
+				+ "	<input type=\"text\" name=\"imee\" value=\""  + osobe.get(Integer.parseInt(request.getQueryString())).getIme()  +  "\" />\r\n"
+				+ "	<label>Prezime:</label>\r\n"
+				+ "	<input type=\"text\" name=\"prezimee\" value=\"" + osobe.get(Integer.parseInt(request.getQueryString())).getPrezime() + "\"/>\r\n"
+				+ "	<button type=\"submit\">Potvrdi</button>\r\n"
+				+ "</form>\r\n"
+				+ "\r\n"
+				+ "</body>\r\n"
+				+ "</html>");
 		
 	}
 
